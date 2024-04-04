7cf3af26cfbc4a7986d155e46b684c404a0beb00
enricobellato <enricobellatodevelopment@gmail.com>
2024-04-04 23:17:51 +0000

    Start migrating to llmosaic

--- logicle/app/api/chat/route.ts
+++ logicle/app/api/chat/route.ts
@@ -1,54 +1,131 @@
 import { LLMStream } from '@/lib/openai'
+import { Message } from '@/types/dto'
 import { getEncoding } from 'js-tiktoken'
 import { getMessages, saveMessage } from 'models/message'
 import { OpenAIMessage } from '@/types/openai'
-import { getConversationWithBackendAssistant } from 'models/conversation'
-import { MessageDTO, Role } from '@/types/chat'
-import ApiResponses from '../utils/ApiResponses'
-import { requireSession } from '../utils/auth'
-import { createResponse } from './utils'
-import { availableToolsForAssistant } from '@/lib/tools/enumerate'
+import Assistants from 'models/assistant'
+import { getConversation } from 'models/conversation'
+import { getBackend } from 'models/backend'
+import { Role } from '@/types/chat'
+//import { getServerSession } from 'next-auth'
+import { NextRequest, NextResponse } from 'next/server'
+//import { authOptions } from '../auth/[...nextauth]/authOptions'
+import ApiErrors from 'app/api/utils/ApiErrors'
+import { nanoid } from 'nanoid'
+//import { auth } from 'auth'
 
 // build a tree from the given message towards root
-function pathToRoot(messages: MessageDTO[], from: MessageDTO): MessageDTO[] {
-  const msgMap = new Map<string, MessageDTO>()
+function pathToRoot(messages: Message[], from: Message): OpenAIMessage[] {
+  const msgMap = new Map<string, Message>()
   messages.forEach((msg) => {
     msgMap[msg.id] = msg
   })
 
-  const list: MessageDTO[] = []
+  const list: OpenAIMessage[] = []
   do {
-    list.push(from)
+    list.push({
+      role: from.role as Role,
+      content: from.content,
+    })
     from = msgMap[from.parent ?? 'none']
   } while (from)
   return list
 }
 
-export const POST = requireSession(async (session, req) => {
-  const userMessage = (await req.json()) as MessageDTO
+const createResponse = (userMessage: Message, stream: ReadableStream<string>) => {
+  // this is what we will write to db and send to the client
+  const assistantMessage: Message = {
+    id: nanoid(),
+    role: 'assistant',
+    content: '',
+    conversationId: userMessage.conversationId,
+    parent: userMessage.id,
+    sentAt: new Date().toISOString(),
+  }
+  const responseStream = new ReadableStream<string>({
+    async start(controller) {
+      let downStreamError = false
+      try {
+        const reader = stream.getReader()
+        const msg = {
+          type: 'response',
+          content: assistantMessage,
+        }
+        controller.enqueue(`data: ${JSON.stringify(msg)} \n\n`)
+        for (;;) {
+          const result = await reader.read()
+          if (result.done) {
+            break
+          }
+          const msg = {
+            type: 'delta',
+            content: result.value,
+          }
+          try {
+            controller.enqueue(`data: ${JSON.stringify(msg)} \n\n`)
+          } catch (e) {
+            console.log(`Exception while sending chat message: ${e}`)
+            downStreamError = true
+            break
+          }
+          // Append the message after sending it to the client.
+          // While it is not possible to keep what we store in db consistent
+          // with what the client sees... it is fairly reasonable to assume
+          // that if we fail to send it, the user has not seen it (But I'm not
+          // sure that this is obvious)
+          assistantMessage.content = assistantMessage.content + result.value
+        }
+      } catch (e) {
+        console.log(`Exception while reading chat message: ${e}`)
+      }
+      // close the stream only if no enqueue() call has failed
+      if (!downStreamError) {
+        try {
+          controller.close()
+        } catch (e) {
+          console.log(`Failed closing controller: ${e}`)
+        }
+      }
+      await saveMessage(assistantMessage)
+    },
+  })
 
-  const conversation = await getConversationWithBackendAssistant(userMessage.conversationId)
+  return new NextResponse(responseStream, {
+    headers: {
+      'Content-Encoding': 'none',
+      'Content-Type': 'text/event-stream',
+    },
+  })
+}
+
+export async function POST(req: NextRequest) {
+  //const session = await getServerSession(authOptions)
+  const userMessage = (await req.json()) as Message
+
+  // TODO: this must be a single query, possibly caching messages
+  const conversation = await getConversation(userMessage.conversationId)
   if (!conversation) {
-    return ApiResponses.invalidParameter(
+    return ApiErrors.invalidParameter(
       `Trying to add a message to a non existing conversation with id ${userMessage.conversationId}`
     )
   }
-  if (conversation.ownerId !== session.user.id) {
-    return ApiResponses.forbiddenAction('Trying to add a message to a non owned conversation')
+
+  const assistant = await Assistants.get(conversation.assistantId)
+  if (!assistant) {
+    return ApiErrors.internalServerError(`Invalid conversation (no assistant)`)
   }
 
-  const dbMessages = await getMessages(userMessage.conversationId)
-  const messageDtos = pathToRoot(dbMessages, userMessage)
-  const messages = messageDtos.map((m) => {
-    return {
-      role: m.role as Role,
-      content: m.content,
-    } as OpenAIMessage
-  })
+  const backend = await getBackend(assistant.backendId)
+  if (!backend) {
+    return ApiErrors.internalServerError('Backend not found')
+  }
 
+  const dbMessages = (await getMessages(userMessage.conversationId)) as Message[] //, session.user.id)
+
+  const messages = pathToRoot(dbMessages, userMessage)
   const encoding = getEncoding('cl100k_base')
 
-  const promptToSend = conversation.systemPrompt!
+  const promptToSend = assistant.systemPrompt
 
   const prompt_tokens = encoding.encode(promptToSend)
 
@@ -61,27 +138,21 @@ export const POST = requireSession(async (session, req) => {
     tokenCount += tokens.length
     messagesToSend = [message, ...messagesToSend]
 
-    if (tokenCount + tokens.length > conversation.tokenLimit!) {
+    if (tokenCount + tokens.length > assistant.tokenLimit) {
       break
     }
   }
 
-  const availableFunctions = (await availableToolsForAssistant(conversation.assistantId)).flatMap(
-    (p) => p.functions
-  )
-  const stream: ReadableStream<string> = LLMStream(
-    conversation.providerType,
-    conversation.endPoint,
-    conversation.model,
-    conversation.apiKey,
-    conversation.assistantId,
+  const stream: ReadableStream<string> = await LLMStream(
+    backend.providerType,
+    backend.endPoint,
+    assistant.model,
+    backend.apiKey,
     promptToSend,
-    conversation.temperature,
-    messagesToSend,
-    messageDtos.toReversed(),
-    availableFunctions
+    assistant.temperature,
+    messagesToSend
   )
   await saveMessage(userMessage)
 
   return createResponse(userMessage, stream)
-})
+}
