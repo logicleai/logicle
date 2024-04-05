6be04084a6d29d8912ecec8ad8be3bce0454f415
enricobellato <enricobellatodevelopment@gmail.com>
2024-04-05 10:26:46 +0000

    Keep implementing

--- logicle/lib/openai/index.ts
+++ logicle/lib/openai/index.ts
@@ -1,17 +1,56 @@
 import { OpenAIMessage } from '@/types/openai'
 import { Provider, ProviderType as LLMosaicProviderType } from '@logicleai/llmosaic'
+import { ChatCompletionCreateParams } from 'openai/resources/chat/completions'
+import { MessageDTO } from '@/types/chat'
 import { ProviderType } from '@/types/provider'
+
+export interface ToolFunction {
+  function: ChatCompletionCreateParams.Function
+  invoke: (
+    messages: MessageDTO[],
+    assistantId: string,
+    params: Record<string, any>
+  ) => Promise<string>
+}
+
+export interface ToolImplementationUploadParams {
+  fileId: string
+  fileName: string
+  contentType: string
+  contentStream: ReadableStream
+  assistantId?: string
+}
+
+export interface ToolImplementationUploadResult {
+  externalId: string
+}
+
+export interface ToolImplementation {
+  functions: ToolFunction[]
+  processFile?: (params: ToolImplementationUploadParams) => Promise<ToolImplementationUploadResult>
+  deleteDocuments?: (docIds: string[]) => Promise<void>
+}
+
+export type ToolBuilder = (params: Record<string, any>) => ToolImplementation
 
 export const LLMStream = async (
   providerType: ProviderType,
   apiHost: string,
   model: string,
   apiKey: string,
+  assistantId: string,
   systemPrompt: string,
   temperature: number,
-  messages: OpenAIMessage[]
+  messages: OpenAIMessage[],
+  messageDtos: MessageDTO[],
+  functions: ToolFunction[]
 ): Promise<ReadableStream<string>> => {
-  const llm = new Provider({apiKey: apiKey, baseUrl: apiHost, providerType: providerType as LLMosaicProviderType})
+  const llm = new Provider({
+    apiKey: apiKey,
+    baseUrl: apiHost,
+    providerType: providerType as LLMosaicProviderType
+  })
+  
   const stream = await llm.completion({
     model: model,
     messages: [
