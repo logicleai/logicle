263cb186604627c0ce8822dc60394d62821d1632
Luca <luca.piccarreta@foosoft.it>
2024-04-09 11:53:38 +0200

    Removed onBeforeUpload listener

--- logicle/app/assistants/components/AssistantForm.tsx
+++ logicle/app/assistants/components/AssistantForm.tsx
@@ -91,18 +91,6 @@ export const AssistantForm = ({ assistant, onSubmit, onChange, fireSubmit }: Pro
     },
   })
 
-  useEffect(() => {
-    const handleBeforeUnload = (event) => {
-      //localStorage.setItem('assistant-form', JSON.stringify(form.getValues()))
-      event.preventDefault()
-      event.returnValue = ''
-    }
-    window.addEventListener('beforeunload', handleBeforeUnload)
-    return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload)
-    }
-  }, [])
-
   const updateModels = (backendId: string) => {
     async function getData() {
       abortController.current?.abort()
