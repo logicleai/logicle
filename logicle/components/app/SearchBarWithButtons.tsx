42c2a671fc65075275110428313ae0f12585a957
Luca <luca.piccarreta@foosoft.it>
2024-04-11 00:34:58 +0200

    Styling search bar

--- logicle/components/app/SearchBarWithButtons.tsx
+++ logicle/components/app/SearchBarWithButtons.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import { Input } from '../ui/input'
+import { IconSearch } from '@tabler/icons-react'
 
 interface Params {
   children: ReactNode
@@ -14,11 +15,16 @@ export const SearchBarWithButtonsOnRight = ({
 }: Params) => {
   return (
     <div className="flex gap-3">
-      <Input
-        className="flex-1"
-        value={searchTerm}
-        onChange={(evt) => onSearchTermChange(evt.target.value)}
-      ></Input>
+      <div className="flex-1 relative">
+        <Input
+          className="flex-1 pl-10"
+          value={searchTerm}
+          onChange={(evt) => onSearchTermChange(evt.target.value)}
+        ></Input>
+        <div className="absolute top-0 bottom-0 left-0 flex items-center ml-2">
+          <IconSearch></IconSearch>
+        </div>
+      </div>
       {children}
     </div>
   )
