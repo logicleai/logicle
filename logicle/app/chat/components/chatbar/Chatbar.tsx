b1029547f96a82a5c04d73baa66beda0d69f13e4
Luca Piccarreta <luca.piccarreta@foosoft.it>
2024-06-23 23:52:26 +0200

    Fix chat grouping in sidebar (#149)
    * Fixed conversation grouping by date
    
    * Removed debug log

--- logicle/app/chat/components/chatbar/Chatbar.tsx
+++ logicle/app/chat/components/chatbar/Chatbar.tsx
@@ -90,14 +90,14 @@ export const Chatbar = () => {
     const conversationsOlder: dto.ConversationWithFolder[] = []
     for (const conversation of conversations) {
       const lastMsgSentAt = conversation.lastMsgSentAt ?? conversation.createdAt
-      if (lastMsgSentAt < currentWeekLimit) {
-        conversationsOlder.push(conversation)
-      } else if (lastMsgSentAt < todayLimit) {
-        conversationsCurrentWeek.push(conversation)
-      } else if (lastMsgSentAt < yesterdayLimit) {
-        conversationsYesterday.push(conversation)
-      } else {
+      if (lastMsgSentAt > todayLimit) {
         conversationsToday.push(conversation)
+      } else if (lastMsgSentAt > yesterdayLimit) {
+        conversationsYesterday.push(conversation)
+      } else if (lastMsgSentAt > currentWeekLimit) {
+        conversationsCurrentWeek.push(conversation)
+      } else {
+        conversationsOlder.push(conversation)
       }
     }
     return {
@@ -128,7 +128,6 @@ export const Chatbar = () => {
 
       {pinnedAssistants.length != 0 && (
         <>
-          <h5 className="text-secondary_text_color">PINS</h5>
           <div className="flex flex-col items-start">
             {pinnedAssistants.map((assistant) => {
               return (
