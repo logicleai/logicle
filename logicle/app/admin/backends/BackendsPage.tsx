ef1f2cc7157428427ab073927dbfac5e393a936f
Luca Piccarreta <luca.piccarreta@foosoft.it>
2024-09-01 02:43:59 +0200

    Prevent creation of unsupported providers (#191)

--- logicle/app/admin/backends/BackendsPage.tsx
+++ logicle/app/admin/backends/BackendsPage.tsx
@@ -101,18 +101,6 @@ export const BackendsPage = () => {
               <DropdownMenuButton onClick={() => onProviderSelect(ProviderType.OpenAI)}>
                 {t('openai-backend')}
               </DropdownMenuButton>
-              <DropdownMenuButton onClick={() => onProviderSelect(ProviderType.Anthropic)}>
-                {t('anthropic-backend')}
-              </DropdownMenuButton>
-              <DropdownMenuButton onClick={() => onProviderSelect(ProviderType.TogetherAI)}>
-                {t('togetherai-backend')}
-              </DropdownMenuButton>
-              <DropdownMenuButton onClick={() => onProviderSelect(ProviderType.Groq)}>
-                {t('groq-backend')}
-              </DropdownMenuButton>
-              <DropdownMenuButton onClick={() => onProviderSelect(ProviderType.LocalAI)}>
-                {t('localai-backend')}
-              </DropdownMenuButton>
               <DropdownMenuButton onClick={() => onProviderSelect(ProviderType.LogicleCloud)}>
                 {t('logiclecloud-backend')}
               </DropdownMenuButton>
