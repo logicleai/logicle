74bf2c3304de320825bea5039d98f5a8855443b2
Luca <luca.piccarreta@foosoft.it>
2024-10-01 19:21:19 +0200

    Editing API keys in Tool form

--- logicle/app/admin/tools/components/ToolForm.tsx
+++ logicle/app/admin/tools/components/ToolForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useTranslation } from 'next-i18next'
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
@@ -12,6 +12,9 @@ import * as dto from '@/types/dto'
 import { ChatGptRetrievalPluginInterface } from '@/lib/tools/chatgpt-retrieval-plugin/interface'
 import { OpenApiPlugin } from '@/lib/tools/openapi/implementation'
 import { Textarea } from '@/components/ui/textarea'
+import OpenAPIParser from '@readme/openapi-parser'
+import { OpenAPIV3 } from 'openapi-types'
+import * as jsYAML from 'js-yaml'
 
 interface Props {
   type: string
@@ -19,15 +22,33 @@ interface Props {
   onSubmit: (tool: dto.UpdateableToolDTO) => void
 }
 
-const configurationSchema = (type: string) => {
+const extractApiKeysFromOpenApiSchema = async (schemaText: string): Promise<string[]> => {
+  console.log('Start schema analysis')
+  const result = new Map<string, OpenAPIV3.SecuritySchemeObject>()
+  const openApiSpecYaml = jsYAML.load(schemaText)
+  const openAPISpec = (await OpenAPIParser.validate(openApiSpecYaml)) as OpenAPIV3.Document
+  if (openAPISpec.components?.securitySchemes) {
+    for (const component in openAPISpec.components.securitySchemes) {
+      const key = component
+      const value = openAPISpec.components.securitySchemes[key] as OpenAPIV3.SecuritySchemeObject
+      if (value.type == 'apiKey') {
+        result.set(key, value as OpenAPIV3.SecuritySchemeObject)
+      }
+    }
+  }
+  return Array.from(result.keys())
+}
+const configurationSchema = (type: string, apiKeys: string[]) => {
   if (type == ChatGptRetrievalPluginInterface.toolName) {
     return z.object({
       baseUrl: z.string().url(),
       apiKey: z.string(),
     })
   } else if (type == OpenApiPlugin.toolName) {
+    const props = Object.fromEntries(apiKeys.map((apiKey) => [apiKey, z.string()]))
     return z.object({
       spec: z.string(),
+      ...props,
     })
   } else {
     // we need a z.any() to make the compiler happy,
@@ -35,12 +56,15 @@ const configurationSchema = (type: string) => {
     return z.any()
   }
 }
+
 const ToolForm: FC<Props> = ({ type, tool, onSubmit }) => {
   const { t } = useTranslation('common')
 
+  const [apiKeys, setApiKeys] = useState<string[]>([])
+
   const formSchema = z.object({
     name: z.string(),
-    configuration: configurationSchema(type),
+    configuration: configurationSchema(type, apiKeys),
   })
 
   type ToolFormFields = z.infer<typeof formSchema>
@@ -49,6 +73,27 @@ const ToolForm: FC<Props> = ({ type, tool, onSubmit }) => {
     resolver: zodResolver(formSchema),
     defaultValues: { ...tool },
   })
+
+  const updateSecurityFields = async () => {
+    const configuration = form.getValues()['configuration']
+    if (configuration && configuration.spec) {
+      const apiKeys = await extractApiKeysFromOpenApiSchema(configuration.spec)
+      console.log(`Got API Keys: ${JSON.stringify(apiKeys)}`)
+      setApiKeys(apiKeys)
+    } else {
+      console.info('No openapi to monitor...')
+    }
+  }
+
+  useEffect(() => {
+    updateSecurityFields()
+  }, [])
+
+  useEffect(() => {
+    form.watch(() => {
+      updateSecurityFields()
+    })
+  }, [form])
 
   const handleSubmit = (values: ToolFormFields) => {
     const v = { ...values }
@@ -98,13 +143,28 @@ const ToolForm: FC<Props> = ({ type, tool, onSubmit }) => {
             name="configuration.spec"
             render={({ field }) => (
               <FormItem label={t('spec')}>
-                <Textarea placeholder={t('spec')} {...field} />
+                <Textarea rows={20} placeholder={t('spec')} {...field} />
               </FormItem>
             )}
           />
+          {apiKeys.map((apiKey) => {
+            return (
+              <>
+                <FormField
+                  key={`configuration.${apiKey}`}
+                  control={form.control}
+                  name={`configuration.${apiKey}`}
+                  render={({ field }) => (
+                    <FormItem label={apiKey}>
+                      <Input placeholder={t('api_key_value')} {...field} />
+                    </FormItem>
+                  )}
+                />
+              </>
+            )
+          })}
         </>
       )}
-
       <Button type="submit">Submit</Button>
     </Form>
   )
