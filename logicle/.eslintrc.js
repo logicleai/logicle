360bf7527efb0ceaa0fd13cc693fdb561976a707
Luca <luca.piccarreta@foosoft.it>
2024-10-12 01:45:38 +0200

    Changed floating promises linting notification to 'error'

--- logicle/.eslintrc.js
+++ logicle/.eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-floating-promises': 'error',
   },
   settings: {
     react: {
