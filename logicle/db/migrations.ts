c10b7ba110eb72008d4f5bfdc5eeef073729ef2e
Luca <luca.piccarreta@foosoft.it>
2024-12-10 01:33:09 +0100

    Fixed migrations

--- logicle/db/migrations.ts
+++ logicle/db/migrations.ts
@@ -34,7 +34,8 @@ export async function migrateToLatest() {
     '20241123-apikeys_and_userprovision': await import(
       './migrations/20241123-apikeys_and_userprovision'
     ),
-    '20241209-assistant_provisioning': await import('./migrations/20241210-file_encryption'),
+    '20241209-assistant_provisioning': await import('./migrations/20241209-assistant_provisioning'),
+    '20241210-file_encryption': await import('./migrations/20241210-file_encryption'),
   }
 
   const dialect = await createDialect()
