eeed095d015a919b10b421908ab2a07921274575
Luca <luca.piccarreta@foosoft.it>
2025-01-31 00:30:39 +0100

    Added o1 / o1mini

--- logicle/lib/chat/models/logicle.ts
+++ logicle/lib/chat/models/logicle.ts
@@ -6,16 +6,18 @@ import {
   claude3OpusModel,
   claude3SonnetModel,
 } from './anthropic'
-import { gpt4oMiniModel, gpt4oModel, gpt35Model } from './openai'
+import { gpt4oMiniModel, gpt4oModel, gpt35Model, o1MiniModel, o1Model } from './openai'
 import { gemini15FlashModel, gemini15ProModel } from './vertex'
 
 export const logicleModels: EnrichedModel[] = [
   gpt4oModel,
   gpt4oMiniModel,
   gpt35Model,
-  {...claude35SonnetModel, id: "claude-3-5-sonnet"},
-  {...claude35HaikuModel, id: "claude-3-5-haiku"},
-  {...claude3OpusModel, id: "claude-3-opus"},
+  o1Model,
+  o1MiniModel,
+  { ...claude35SonnetModel, id: 'claude-3-5-sonnet' },
+  { ...claude35HaikuModel, id: 'claude-3-5-haiku' },
+  { ...claude3OpusModel, id: 'claude-3-opus' },
   claude3SonnetModel,
   claude3HaikuModel,
   gemini15ProModel,
