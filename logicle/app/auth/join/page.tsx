2321537fbc889481145ca98772247f87dd6c4067
Luca Piccarreta <luca.piccarreta@foosoft.it>
2025-02-03 22:51:13 +0100

    Prevent robot indexing (#302)
    * Add robots noindex / nofollow to login page metadata
    
    * Robot info also for signup page

--- logicle/app/auth/join/page.tsx
+++ logicle/app/auth/join/page.tsx
@@ -1,9 +1,13 @@
 import Signup from './SignUpPanel'
-import { Metadata } from 'next';
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Sign up',
-};
+  robots: {
+    index: false,
+    follow: false,
+  },
+}
 
 export default async function SignUpPage() {
   return <Signup />
