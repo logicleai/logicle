477b7c367210e758ef1870263e9c92106a201ab5
Luca <luca.piccarreta@foosoft.it>
2025-02-13 16:28:41 +0100

    Internationalized tool name + hidden File tool from UI

--- logicle/app/context/client.ts
+++ logicle/app/context/client.ts
@@ -8,10 +8,19 @@ export function initi18n() {
   void i18next
     .use(initReactI18next)
     .use(
-      resourcesToBackend(
-        (language: string, namespace: string) =>
-          import(`../../locales/${language}/${namespace}.json`)
-      )
+      resourcesToBackend((language: string, namespace: string) => {
+        if (namespace == 'all') {
+          return Promise.all([
+            import(`../../locales/${language}/logicle.json`),
+            import(`../../locales/${language}/tools.json`),
+          ]).then(([logicleModule, toolsModule]) => ({
+            ...logicleModule.default,
+            ...toolsModule.default,
+          }))
+        } else {
+          return import(`../../locales/${language}/${namespace}.json`)
+        }
+      })
     )
     .init({
       lng: 'en', // let detect the language on client side
@@ -19,6 +28,6 @@ export function initi18n() {
         order: ['path', 'htmlTag', 'cookie', 'navigator'],
       },
       preload: [],
-      defaultNS: 'logicle',
+      defaultNS: 'all',
     })
 }
--- logicle/lib/tools/chatgpt-retrieval-plugin/interface.ts
+++ logicle/lib/tools/chatgpt-retrieval-plugin/interface.ts
@@ -1,10 +1,8 @@
-import { ToolType } from '@/lib/tools/tools'
-
 export interface ChatGptRetrievalPluginParams {
   baseUrl: string
   apiKey: string
 }
 
 export class ChatGptRetrievalPluginInterface {
-  static toolName: ToolType = 'chatgpt-retrieval-plugin'
+  static toolName: string = 'chatgpt-retrieval-plugin'
 }
