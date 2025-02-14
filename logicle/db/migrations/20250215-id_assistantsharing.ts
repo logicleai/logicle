a8811dc673b5a20be9538b840d807fae5feecc54
Luca <luca.piccarreta@foosoft.it>
2025-02-15 00:33:11 +0100

    Add id column to AssistantSharing Table

---
+++ logicle/db/migrations/20250215-id_assistantsharing.ts
@@ -0,0 +1,44 @@
+import { Kysely, sql } from 'kysely'
+import { nanoid } from 'nanoid'
+
+const string = 'text'
+
+export async function up(db: Kysely<any>): Promise<void> {
+  const sharingList = await db.selectFrom('AssistantSharing').selectAll().execute()
+  await db.schema.dropTable('AssistantSharing').execute()
+  await db.schema
+    .createTable('AssistantSharing')
+    .addColumn('id', string, (col) => col.notNull().primaryKey())
+    .addColumn('assistantId', string, (col) => col.notNull())
+    .addColumn('workspaceId', string)
+    .addForeignKeyConstraint(
+      'fk_AssistantSharing_Assistant',
+      ['assistantId'],
+      'Assistant',
+      ['id'],
+      (cb) => cb.onDelete('cascade')
+    )
+    .addForeignKeyConstraint(
+      'fk_AssistantSharing_Workspace',
+      ['workspaceId'],
+      'Workspace',
+      ['id'],
+      (cb) => cb.onDelete('cascade')
+    )
+    .execute()
+  await db.schema
+    .createIndex('AssistantSharing_assistantId_workspaceId')
+    .on('AssistantSharing')
+    .columns(['assistantId', 'workspaceId'])
+    .execute()
+
+  if (sharingList.length != 0) {
+    const sharingListWithId = sharingList.map((v) => {
+      return {
+        id: nanoid(),
+        ...v,
+      }
+    })
+    await db.insertInto('AssistantSharing').values(sharingListWithId).execute()
+  }
+}
