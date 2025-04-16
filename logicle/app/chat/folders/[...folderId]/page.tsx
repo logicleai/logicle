28cf5e512e57ef8bece84c19f2c3c58f713bec69
Luca <luca.piccarreta@foosoft.it>
2025-04-16 13:10:42 +0200

    A minum of layout...

--- logicle/app/chat/folders/[...folderId]/page.tsx
+++ logicle/app/chat/folders/[...folderId]/page.tsx
@@ -4,22 +4,47 @@ import ChatPageContext from '@/app/chat/components/context'
 import { useParams } from 'next/navigation'
 import { useSWRJson } from '@/hooks/swr'
 import * as dto from '@/types/dto'
+import WithLoadingAndError from '@/components/ui/WithLoadingAndError'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { useTranslation } from 'react-i18next'
 
-const FolderPage = () => {
+const ChatFolderPage = () => {
   const {} = useContext(ChatPageContext)
-
+  const { t } = useTranslation()
   const { folderId } = useParams() as { folderId: string }
-  const { data: conversations } = useSWRJson<dto.Conversation[]>(
-    `/api/user/folders/${folderId}/conversations`
-  )
+
+  const { data: folder } = useSWRJson<dto.ConversationFolder>(`/api/user/folders/${folderId}`)
+
+  const {
+    data: conversations,
+    isLoading,
+    error,
+  } = useSWRJson<dto.Conversation[]>(`/api/user/folders/${folderId}/conversations`)
 
   return (
-    <div className="flex-1 flex flex-col overflow-hidden">
-      {(conversations || []).map((c) => {
-        return <div key={c.id}>{c.name}</div>
-      })}
-    </div>
+    <WithLoadingAndError isLoading={isLoading} error={error}>
+      <div className="flex flex-1 flex-col gap-2 items-center px-4 py-6">
+        <div className="max-w-[960px] w-3/4 h-full flex flex-col gap-3">
+          <div className="flex justify-between items-center">
+            <h1 className="mb-4">{`${t('folder')} ${folder?.name ?? ''}`}</h1>
+          </div>
+          <ScrollArea className="flex-1 min-h-0">
+            <div className=" gap-4 flex flex-col">
+              {(conversations ?? []).map((conversation) => {
+                return (
+                  <div key={conversation.id} className="flex group align-center gap-2 items-center">
+                    <div className="flex flex-col flex-1 h-full">
+                      <div className="font-bold">{conversation.name}</div>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </ScrollArea>
+        </div>
+      </div>
+    </WithLoadingAndError>
   )
 }
 
-export default FolderPage
+export default ChatFolderPage
