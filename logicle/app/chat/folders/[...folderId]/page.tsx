b76f0026b8cc7d852c9ab2ac58c8166497a14602
Luca <luca.piccarreta@foosoft.it>
2025-04-16 14:41:11 +0200

    Link chats in folder to chat page

--- logicle/app/chat/folders/[...folderId]/page.tsx
+++ logicle/app/chat/folders/[...folderId]/page.tsx
@@ -1,17 +1,19 @@
 'use client'
 import React, { useContext, useEffect } from 'react'
 import ChatPageContext from '@/app/chat/components/context'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import { useSWRJson } from '@/hooks/swr'
 import * as dto from '@/types/dto'
 import WithLoadingAndError from '@/components/ui/WithLoadingAndError'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { useTranslation } from 'react-i18next'
+import { Button } from '@/components/ui/button'
 
 const ChatFolderPage = () => {
   const {} = useContext(ChatPageContext)
   const { t } = useTranslation()
   const { folderId } = useParams() as { folderId: string }
+  const router = useRouter()
 
   const { data: folder } = useSWRJson<dto.ConversationFolder>(`/api/user/folders/${folderId}`)
 
@@ -21,6 +23,9 @@ const ChatFolderPage = () => {
     error,
   } = useSWRJson<dto.Conversation[]>(`/api/user/folders/${folderId}/conversations`)
 
+  const handleClick = (conversation: dto.Conversation) => {
+    router.push(`/chat/${conversation.id}`)
+  }
   return (
     <WithLoadingAndError isLoading={isLoading} error={error}>
       <div className="flex flex-1 flex-col gap-2 items-center px-4 py-6">
@@ -32,11 +37,16 @@ const ChatFolderPage = () => {
             <div className=" gap-4 flex flex-col">
               {(conversations ?? []).map((conversation) => {
                 return (
-                  <div key={conversation.id} className="flex group align-center gap-2 items-center">
-                    <div className="flex flex-col flex-1 h-full">
+                  <Button
+                    variant="ghost"
+                    key={conversation.id}
+                    className="flex group align-center gap-2 items-center"
+                    onClick={() => handleClick(conversation)}
+                  >
+                    <div className="flex flex-col flex-1 h-full text-left">
                       <div className="font-bold">{conversation.name}</div>
                     </div>
-                  </div>
+                  </Button>
                 )
               })}
             </div>
