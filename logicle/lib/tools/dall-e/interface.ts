45fa6f1b0691ae264159df26979dd341499c598a
Luca <luca.piccarreta@foosoft.it>
2025-04-28 19:38:57 +0200

    Editing of ImageGeneration tool

--- logicle/lib/tools/dall-e/interface.ts
+++ logicle/lib/tools/dall-e/interface.ts
@@ -1,9 +1,18 @@
-export type Model = 'dall-e-2' | 'dall-e-3' | 'gpt-image-1'
+import * as z from 'zod'
+
+export const Dall_eModels = ['dall-e-2', 'dall-e-3', 'gpt-image-1'] as const
+
+export type Model = (typeof Dall_eModels)[number]
 
 export interface Dall_ePluginParams {
   apiKey: string
   model?: Model
 }
+
+export const Dall_eSchema = z.object({
+  apiKey: z.string(),
+  model: z.enum(Dall_eModels),
+})
 
 export class Dall_ePluginInterface {
   static toolName: string = 'dall-e'
