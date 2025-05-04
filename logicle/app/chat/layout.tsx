f7ab6c3950218d351a5f5eee97eb7f535ef084f6
Luca Piccarreta <luca.piccarreta@foosoft.it>
2025-05-04 14:29:28 +0200

    Move chat state to app level (#414)

--- logicle/app/chat/layout.tsx
+++ logicle/app/chat/layout.tsx
@@ -1,15 +1,6 @@
-import { ChatPageContextProvider } from '@/app/chat/components/ChatPageContextProvider'
-import { ChatPageState, defaultChatPageState } from '@/app/chat/components/state'
 import { MainLayout } from '../layouts/MainLayout'
 import { Chatbar } from './components/Chatbar'
 
 export default function ChatLayout({ children }) {
-  const initialState: ChatPageState = {
-    ...defaultChatPageState,
-  }
-  return (
-    <ChatPageContextProvider initialState={initialState}>
-      <MainLayout leftBar={<Chatbar />}>{children}</MainLayout>
-    </ChatPageContextProvider>
-  )
+  return <MainLayout leftBar={<Chatbar />}>{children}</MainLayout>
 }
