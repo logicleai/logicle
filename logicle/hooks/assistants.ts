ae544862fc2b8c95c63ce2c2b0d85bd4790e0424
Luca <luca.piccarreta@foosoft.it>
2025-05-14 19:19:10 +0200

    Removed unused function

--- logicle/hooks/assistants.ts
+++ logicle/hooks/assistants.ts
@@ -2,15 +2,6 @@ import * as dto from '@/types/dto'
 import { mutate } from 'swr'
 import { useSWRJson } from './swr'
 
-export const useAssistant = (assistantId: string) => {
-  const { data, error, isLoading } = useSWRJson<dto.Assistant>(`/api/assistants/${assistantId}`)
-  return {
-    isLoading,
-    isError: error,
-    assistant: data,
-  }
-}
-
 export const useAssistants = () => {
   const url = `/api/assistants`
   return useSWRJson<dto.AssistantWithOwner[]>(url)
