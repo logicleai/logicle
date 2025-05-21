41adcf1c54ca4d0aba725d62aa9bf9d43623093d
Luca Piccarreta <luca.piccarreta@foosoft.it>
2025-05-21 23:05:09 +0200

    Websearch api url can now be configured (#462)

--- logicle/lib/tools/websearch/interface.ts
+++ logicle/lib/tools/websearch/interface.ts
@@ -2,10 +2,12 @@ import * as z from 'zod'
 
 export interface WebSearchParams {
   apiKey: string
+  apiUrl?: string | null
 }
 
 export const WebSearchSchema = z.object({
   apiKey: z.string(),
+  apiUrl: z.string().nullable(),
 })
 
 export class WebSearchInterface {
