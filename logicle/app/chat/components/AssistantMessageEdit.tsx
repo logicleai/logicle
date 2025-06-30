a1ab85522cbcf180e2d260b195e9c46f148130d6
Luca <luca.piccarreta@foosoft.it>
2025-06-30 02:03:43 +0200

    We now.... have a preview!

--- logicle/app/chat/components/AssistantMessageEdit.tsx
+++ logicle/app/chat/components/AssistantMessageEdit.tsx
@@ -1,7 +1,12 @@
 import { Button } from '@/components/ui/button'
-import { TextareaHTMLAttributes, useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { string } from 'zod/v4'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { AssistantMessageMarkdown } from './AssistantMessageMarkdown'
+import { ScrollArea } from '@/components/ui/scroll-area'
+
+const tabs = ['edit', 'preview'] as const
+type TabId = (typeof tabs)[number]
 
 interface Props {
   initialText: string
@@ -11,26 +16,48 @@ interface Props {
 export const AssistantMessageEdit = ({ onClose, initialText, height }: Props) => {
   const { t } = useTranslation()
   const [text, setText] = useState<string>(initialText)
+  const [activeTab, setActiveTab] = useState<TabId>('edit')
   const handleSave = () => {}
 
   return (
     <>
-      <textarea style={{ height }} onChange={(evt) => setText(evt.target.value)}>
-        {text}
-      </textarea>
-      <div className="mt-4 flex justify-center gap-4">
-        <Button variant="primary" onClick={handleSave}>
-          {t('save_and_submit')}
-        </Button>
-        <Button
-          variant="secondary"
-          onClick={() => {
-            onClose()
-          }}
-        >
-          {t('cancel')}
-        </Button>
+      <div className="flex flex-horz justify-between">
+        <Tabs value={activeTab} onValueChange={(value) => setActiveTab(value as TabId)}>
+          <TabsList>
+            {tabs.map((menu) => {
+              return (
+                <TabsTrigger role="tab" key={menu} value={menu}>
+                  {t(menu)}
+                </TabsTrigger>
+              )
+            })}
+          </TabsList>
+        </Tabs>
+        <div>
+          <Button variant="primary" onClick={handleSave}>
+            {t('save_and_submit')}
+          </Button>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              onClose()
+            }}
+          >
+            {t('cancel')}
+          </Button>
+        </div>
       </div>
+      {activeTab == 'edit' && (
+        <textarea style={{ height }} onChange={(evt) => setText(evt.target.value)}>
+          {text}
+        </textarea>
+      )}
+
+      {activeTab == 'preview' && (
+        <ScrollArea style={{ height }} className="border p-3">
+          <AssistantMessageMarkdown className="prose">{text}</AssistantMessageMarkdown>
+        </ScrollArea>
+      )}
     </>
   )
 }
