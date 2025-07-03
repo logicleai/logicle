b1a0bdd5172ff3a4a6a43add1bbc46c142091623
Luca Piccarreta <luca.piccarreta@foosoft.it>
2025-07-03 18:18:04 +0200

    Fix blank system prompt in asssitant informations (#515)

--- logicle/app/api/user/assistants/[assistantId]/systemPrompt/route.ts
+++ logicle/app/api/user/assistants/[assistantId]/systemPrompt/route.ts
@@ -1,4 +1,4 @@
-import { getAssistantVersion } from 'models/assistant'
+import { getAssistantVersion, getPublishedAssistantVersion } from 'models/assistant'
 import ApiResponses from '@/api/utils/ApiResponses'
 import { requireSession, SimpleSession } from '@/app/api/utils/auth'
 import { NextRequest } from 'next/server'
@@ -8,7 +8,7 @@ export const dynamic = 'force-dynamic'
 export const GET = requireSession(
   async (session: SimpleSession, req: NextRequest, params: { assistantId: string }) => {
     const assistantId = params.assistantId
-    const assistant = await getAssistantVersion(assistantId)
+    const assistant = await getPublishedAssistantVersion(assistantId)
     if (!assistant) {
       return ApiResponses.noSuchEntity()
     }
