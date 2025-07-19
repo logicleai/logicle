06920de9d54847a187cd53f3ff095d4a11c2b2b4
Luca <luca.piccarreta@foosoft.it>
2025-07-19 16:46:03 +0200

    Added "usable" flag to UserAssistant listing API

--- logicle/app/api/user/tools/route.ts
+++ logicle/app/api/user/tools/route.ts
@@ -3,18 +3,25 @@ import { requireSession } from '../../utils/auth'
 import { getTools } from '@/models/tool'
 import { getUserWorkspaceMemberships } from '@/models/user'
 import * as dto from '@/types/dto'
+import { isToolVisible } from '@/lib/rbac'
 
 export const dynamic = 'force-dynamic'
 
 // Fetch prompts
 export const GET = requireSession(async (session) => {
-  const tools = (await getTools()).map((t) => {
+  const workspaceMemberships = await getUserWorkspaceMemberships(session.userId)
+  const tools = (await getTools()).map((tool) => {
     return {
-      id: t.id,
-      name: t.name,
-      provisioned: t.provisioned,
-      capability: t.capability,
-    }
+      id: tool.id,
+      name: tool.name,
+      provisioned: tool.provisioned,
+      capability: tool.capability,
+      visible: isToolVisible(
+        tool,
+        session.userRole,
+        workspaceMemberships.map((m) => m.id)
+      ),
+    } satisfies dto.AssistantTool
   })
   return ApiResponses.json(tools)
 })
