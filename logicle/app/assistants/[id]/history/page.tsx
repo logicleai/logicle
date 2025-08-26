57252d6d15336ecc7d69aa4068d84fccc802abd3
Luca <luca.piccarreta@foosoft.it>
2025-08-26 20:10:02 +0200

    Extract AssistantHistoryEntry from AssistantHistory

--- logicle/app/assistants/[id]/history/page.tsx
+++ logicle/app/assistants/[id]/history/page.tsx
@@ -5,9 +5,95 @@ import { useSWRJson } from '@/hooks/swr'
 import { AssistantVersion } from '@/db/schema'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import * as dto from '@/types/dto'
-import { AssistantForm } from '../../components/AssistantForm'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { useEffect, useState } from 'react'
 import { get } from '@/lib/fetch'
+import { useTranslation } from 'react-i18next'
+import { GeneralTabPanel } from '../../components/GeneralTabPanel'
+import { SystemPromptTabPanel } from '../../components/SystemPromptTabPanel'
+import { ToolsTabPanel } from '../../components/ToolsTabPanel'
+import { KnowledgeTabPanel } from '../../components/KnowledgeTabPanel'
+import { FormProvider, useForm, UseFormReturn } from 'react-hook-form'
+import { DEFAULT, FormFields, formSchema } from '../../components/AssistantFormField'
+import { useEnvironment } from '@/app/context/environmentProvider'
+import { useBackendsModels } from '@/hooks/backends'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+type TabState = 'general' | 'instructions' | 'tools' | 'knowledge'
+
+const AssistantHistoryEntry = ({ assistantVersion }: { assistantVersion: dto.AssistantDraft }) => {
+  const { t } = useTranslation()
+  const [activeTab, setActiveTab] = useState<TabState>('general')
+  const environment = useEnvironment()
+  const { data: models } = useBackendsModels()
+  const backendModels = models || []
+
+  const isToolCallingModel = (modelId: string) => {
+    return environment.models.find((m) => m.id === modelId)?.capabilities.function_calling === true
+  }
+
+  const initialValues = {
+    ...assistantVersion,
+    reasoning_effort: assistantVersion.reasoning_effort ?? DEFAULT,
+    model: `${assistantVersion.model}#${assistantVersion.backendId}`,
+    backendId: undefined,
+    name: 'ciao',
+  } as FormFields
+
+  const resolver = zodResolver(formSchema)
+  const form = useForm<FormFields>({
+    resolver,
+    values: initialValues,
+    disabled: true,
+  })
+
+  return (
+    <FormProvider {...form}>
+      <div className="flex-1">
+        <div
+          onSubmit={(e) => e.preventDefault()}
+          className="space-y-6 h-full flex flex-col p-2 overflow-hidden min-h-0 "
+        >
+          <div className="flex flex-row gap-1 self-center">
+            <Tabs
+              onValueChange={(value) => setActiveTab(value as TabState)}
+              value={activeTab}
+              className="space-y-4 h-full flex flex-col Tabs"
+            >
+              <TabsList>
+                <TabsTrigger value="general">{t('general')}</TabsTrigger>
+                <TabsTrigger value="instructions">{t('instructions')}</TabsTrigger>
+                {isToolCallingModel(form.getValues().model.split('#')[0]) && (
+                  <TabsTrigger value="tools">{t('tools')}</TabsTrigger>
+                )}
+                <TabsTrigger value="knowledge">{t('knowledge')}</TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </div>
+
+          <GeneralTabPanel
+            className="flex-1 min-w-0"
+            form={form}
+            backendModels={backendModels}
+            visible={activeTab === 'general'}
+          />
+          <SystemPromptTabPanel
+            className="flex-1 min-h-0"
+            form={form}
+            visible={activeTab === 'instructions'}
+          ></SystemPromptTabPanel>
+          <ToolsTabPanel className="flex-1 min-w-0" form={form} visible={activeTab === 'tools'} />
+          <KnowledgeTabPanel
+            className="flex-1"
+            form={form}
+            visible={activeTab === 'knowledge'}
+            assistant={assistantVersion}
+          />
+        </div>
+      </div>
+    </FormProvider>
+  )
+}
 
 const AssistantHistory = () => {
   const { id } = useParams() as { id: string }
@@ -16,7 +102,6 @@ const AssistantHistory = () => {
   const [assistantVersionId, setAssistantVersionId] = useState<string | undefined>()
   const [assistantVersion, setAssistantVersion] = useState<dto.AssistantDraft | undefined>()
   const assistantVersions = data ?? []
-
   useEffect(() => {
     const doLoad = async () => {
       if (assistantVersionId) {
@@ -31,6 +116,7 @@ const AssistantHistory = () => {
     }
     void doLoad()
   }, [assistantVersionId])
+
   return (
     <div className="flex">
       <ScrollArea className="scroll-workaround h-full p-2 w-200">
@@ -56,7 +142,7 @@ const AssistantHistory = () => {
       </ScrollArea>
       <div className="flex-1">
         {assistantVersion && (
-          <AssistantForm assistant={assistantVersion} onSubmit={() => {}}></AssistantForm>
+          <AssistantHistoryEntry assistantVersion={assistantVersion}></AssistantHistoryEntry>
         )}
       </div>
     </div>
