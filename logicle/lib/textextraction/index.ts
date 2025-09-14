4062af0c2797249fb30858fd64cb63b7af221cc1
Luca Piccarreta <luca.piccarreta@foosoft.it>
2025-09-14 23:51:40 +0200

    Extraction of all sheets from Excel files (#595)

--- logicle/lib/textextraction/index.ts
+++ logicle/lib/textextraction/index.ts
@@ -1,22 +1,13 @@
-import ExcelJS from 'exceljs'
-import { ensureABView } from '../utils'
-import { sheetToMarkdown } from './xlstomarkdown'
 import micromatch from 'micromatch'
 import { pdfExtractor } from './pdf'
 import { wordExtractor } from './word'
+import { excelExtractor } from './excel'
 
 export interface TextExtractionOption {
   keepImages?: boolean
 }
 
 export type TextExtractor = (buffer: Buffer, options?: TextExtractionOption) => Promise<string>
-
-const excelExtractor: TextExtractor = async (data: Buffer) => {
-  const wb = new ExcelJS.Workbook()
-  await wb.xlsx.load(ensureABView(data).buffer)
-  const ws = wb.worksheets[0]
-  return sheetToMarkdown(ws, { headerRow: 1 })
-}
 
 const genericTextExtractor: TextExtractor = async (data: Buffer) => {
   return data.toString('utf8')
