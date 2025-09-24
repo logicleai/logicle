90ba168bf74b56ddd65602d254cbee6144266b1b
Luca <luca.piccarreta@foosoft.it>
2025-09-25 01:42:36 +0200

    Using streamable http transport when url ends with /mcp

--- logicle/lib/tools/mcp/implementation.ts
+++ logicle/lib/tools/mcp/implementation.ts
@@ -24,36 +24,38 @@ interface CacheItem {
 
 const clientCache = new Map<string, CacheItem>()
 
-async function createTransport(url: string) {
+const createTransport = (url: string) => {
   if (url.endsWith('/mcp')) {
+    logger.info(`Create MCP streamable http transport for url ${url}`)
     return new StreamableHTTPClientTransport(new URL(url))
   } else {
+    logger.info(`Create MCP SSE transport for url ${url}`)
     return new SSEClientTransport(new URL(url))
   }
 }
 
-async function getClient(url: string) {
+const getClient = async (url: string) => {
   const cached = clientCache.get(url)
   if (cached) {
     return cached.client
-  }
-  const transport = createTransport(url)
-  transport.onclose = () => {
-    logger.info('MCP-SSE Transport closed')
-  }
-  transport.onerror = (error) => {
-    logger.error(
-      `MCP-SSE Transport error, closing and removing from cache client for tool at ${url}`,
-      error
-    )
-    void client.close()
-    clientCache.delete(url)
   }
   logger.info(`Creating MCP client to ${url}`)
   const client = new Client({
     name: 'example-client',
     version: '1.0.0',
   })
+  const transport = createTransport(url)
+  transport.onclose = () => {
+    logger.info('MCP Transport closed')
+  }
+  transport.onerror = (error) => {
+    logger.error(
+      `MCP Transport error, closing and removing from cache client for tool at ${url}`,
+      error
+    )
+    void client.close()
+    clientCache.delete(url)
+  }
   try {
     await client.connect(transport)
   } catch (e) {
