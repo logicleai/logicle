c6ef69edea923e7b0df11bfe7ad27589a6f29c0a
Luca <luca.piccarreta@foosoft.it>
2025-09-28 02:17:44 +0200

    Native google search

---
+++ logicle/lib/tools/google.web_search/implementation.ts
@@ -0,0 +1,20 @@
+import { ToolBuilder, ToolFunctions, ToolImplementation, ToolParams } from '@/lib/chat/tools'
+import { GoogleWebSearchInterface } from './interface'
+
+export class GoogleWebSearch extends GoogleWebSearchInterface implements ToolImplementation {
+  static builder: ToolBuilder = (toolParams: ToolParams) => new GoogleWebSearch(toolParams)
+  supportedMedia = []
+  constructor(public toolParams: ToolParams) {
+    super()
+  }
+
+  async functions(): Promise<ToolFunctions> {
+    return {
+      google_search: {
+        type: 'provider-defined',
+        id: 'google.google_search',
+        args: {},
+      },
+    }
+  }
+}
