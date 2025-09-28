11ebdfc72f803f7fe73b1a5cc80da541a901ca6e
Luca <luca.piccarreta@foosoft.it>
2025-09-28 02:44:09 +0200

    Added model gemini-2.5-pro

--- logicle/lib/chat/models/gemini.ts
+++ logicle/lib/chat/models/gemini.ts
@@ -1,10 +1,15 @@
 import { LlmModel } from '.'
-import { gemini25FlashModel } from './vertex'
+import { gemini25FlashModel, gemini25ProModel } from './vertex'
 
 export const geminiModels: LlmModel[] = [
   {
     ...gemini25FlashModel,
     id: 'gemini-2.5-flash',
+    provider: 'gemini',
+  },
+  {
+    ...gemini25ProModel,
+    id: 'gemini-2.5-pro',
     provider: 'gemini',
   },
 ]
