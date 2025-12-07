a36ebd5dca21948072ab6aa357140f5caa4cf4a7
Luca Piccarreta <luca.piccarreta@foosoft.it>
2025-12-07 11:04:02 +0100

    Fixed login page

--- logicle/app/auth/login/page.tsx
+++ logicle/app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import env from '@/lib/env'
 import Login from './LoginPanel'
 import { Metadata } from 'next'
 import { getUserCount } from '@/models/user'
-import { listIdentityProvidersRaw } from '@/lib/auth/saml'
+import { jsonToIdentityProvider, listIdentityProvidersRaw } from '@/lib/auth/saml'
 
 export const metadata: Metadata = {
   title: 'Login',
@@ -17,18 +17,9 @@ export default async function LoginPage() {
   const identityProviders = await listIdentityProvidersRaw()
   const enableSignup = env.signup.enable || userCount === 0
   const connectionsFormatted = identityProviders.map((connection) => {
-    const idpMetadata = 'idpMetadata' in connection ? connection.idpMetadata : undefined
-    const oidcProvider = 'oidcProvider' in connection ? connection.oidcProvider : undefined
-
-    const name =
-      connection.name ||
-      (idpMetadata
-        ? idpMetadata.friendlyProviderName || idpMetadata.provider
-        : `${oidcProvider?.provider}`)
-
     return {
-      clientID: connection.clientID,
-      name,
+      clientID: connection.key,
+      name: connection.data.name ?? '',
     }
   })
   return <Login connections={connectionsFormatted} enableSignup={enableSignup} />
