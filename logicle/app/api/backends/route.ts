649106de9e07993f1d16c95f4ac654b71e91c545
Luca Piccarreta <luca.piccarreta@foosoft.it>
2026-01-04 03:51:39 +0100

    Use zod for backend API validation

--- logicle/app/api/backends/route.ts
+++ logicle/app/api/backends/route.ts
@@ -3,6 +3,8 @@ import { requireAdmin, requireSession } from '@/api/utils/auth'
 import ApiResponses from '@/api/utils/ApiResponses'
 import { protectApiKey } from '@/types/secure'
 import env from '@/lib/env'
+import * as dto from '@/types/dto'
+import { insertableBackendSchema } from '@/types/validation/backend'
 
 export const GET = requireSession(async () => {
   const backends = await getBackends()
@@ -14,8 +16,13 @@ export const POST = requireAdmin(async (req: Request) => {
   if (env.backends.locked) {
     return ApiResponses.forbiddenAction('Unable to create the backen: configuration locked')
   }
+
+  const result = insertableBackendSchema.safeParse(await req.json())
+  if (!result.success) {
+    return ApiResponses.invalidParameter('Invalid body', result.error.format())
+  }
   try {
-    const created = await createBackend(await req.json())
+    const created = await createBackend(result.data)
     return ApiResponses.created(created)
   } catch {
     return ApiResponses.internalServerError('Creation failed')
--- logicle/types/validation/backend.ts
+++ logicle/types/validation/backend.ts
@@ -4,36 +4,33 @@ const zodApiKey = z.string().min(2, { message: 'Api Key  must be at least 2 char
 const zodName = z.string().min(2, { message: 'Backend name must be at least 2 characters.' })
 const zodCredentials = z.string().min(2, { message: 'Credentials must be at least 2 characters.' })
 
-export const insertableBackendSchema = z.discriminatedUnion('providerType', [
-  z.object({
-    providerType: z.literal('openai'),
-    name: zodName,
-    apiKey: zodApiKey,
-  }),
-  z.object({
-    providerType: z.literal('anthropic'),
-    name: zodName,
-    apiKey: zodApiKey,
-  }),
+const options = [
+  z.object({ providerType: z.literal('openai'), name: zodName, apiKey: zodApiKey }),
+  z.object({ providerType: z.literal('anthropic'), name: zodName, apiKey: zodApiKey }),
   z.object({
     providerType: z.literal('logiclecloud'),
     name: zodName,
     apiKey: zodApiKey,
     endPoint: z.string().url(),
   }),
-  z.object({
-    providerType: z.literal('gcp-vertex'),
-    name: zodName,
-    credentials: zodCredentials,
-  }),
-  z.object({
-    providerType: z.literal('perplexity'),
-    name: zodName,
-    apiKey: zodApiKey,
-  }),
-  z.object({
-    providerType: z.literal('gemini'),
-    name: zodName,
-    apiKey: zodApiKey,
-  }),
-])
+  z.object({ providerType: z.literal('gcp-vertex'), name: zodName, credentials: zodCredentials }),
+  z.object({ providerType: z.literal('perplexity'), name: zodName, apiKey: zodApiKey }),
+  z.object({ providerType: z.literal('gemini'), name: zodName, apiKey: zodApiKey }),
+] as const
+
+export const insertableBackendSchema = z.discriminatedUnion('providerType', options)
+
+// An "optional" version of the insertable schema for updates
+// The discriminator must be re-required in each option, in order
+// to be able to perform validation
+export const updateableBackendSchema = z.discriminatedUnion(
+  'providerType',
+  options.map((o) =>
+    o.partial().extend({
+      providerType: o.shape.providerType, // re-require discriminator
+    })
+  ) as unknown as typeof options
+)
+
+export type InsertableBackend = z.infer<typeof insertableBackendSchema>
+export type UpdateableBackend = z.infer<typeof updateableBackendSchema>
