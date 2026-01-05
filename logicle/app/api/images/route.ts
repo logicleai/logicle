421557d06e1d6b13ed9faf64370990661cd4beb5
Luca Piccarreta <luca.piccarreta@foosoft.it>
2026-01-05 14:15:25 +0100

    Validate file creation API

--- logicle/app/api/images/route.ts
+++ logicle/app/api/images/route.ts
@@ -1,14 +1,18 @@
 import { nanoid } from 'nanoid'
 import ApiResponses from '../utils/ApiResponses'
 import { requireSession } from '../utils/auth'
-import * as dto from '@/types/dto'
 import { addFile } from '@/models/file'
 import { addAssistantFile } from '@/models/assistant'
 import env from '@/lib/env'
+import { insertableFileSchema } from '@/types/dto/file'
 
 export const POST = requireSession(async (_session, req) => {
   const id = nanoid()
-  const file = (await req.json()) as dto.InsertableFile
+  const result = insertableFileSchema.safeParse(await req.json())
+  if (!result.success) {
+    return ApiResponses.invalidParameter('Invalid body', result.error.format())
+  }
+  const file = result.data
   const assistantId = req.nextUrl.searchParams.get('assistantId')
   const path = `${id}-${file.name.replace(/(\W+)/gi, '-')}`
   const created = await addFile(file, path, env.fileStorage.encryptFiles)
