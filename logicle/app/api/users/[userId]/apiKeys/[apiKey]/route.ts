0b6614cf13c910a1b9ec01f7bc7ac57608e7b39f
Luca Piccarreta <luca.piccarreta@foosoft.it>
2026-01-06 20:43:07 +0100

    Migrate API keys APIs

--- logicle/app/api/users/[userId]/apiKeys/[apiKey]/route.ts
+++ logicle/app/api/users/[userId]/apiKeys/[apiKey]/route.ts
@@ -1,4 +1,3 @@
-import { requireAdmin } from '@/api/utils/auth'
 import ApiResponses from '@/api/utils/ApiResponses'
 import { deleteApiKey, getUserApiKey } from '@/models/apikey'
 import {
@@ -7,36 +6,42 @@ import {
   KnownDbError,
   KnownDbErrorCode,
 } from '@/db/exception'
+import { route, operation } from '@/lib/routes'
 
-export const DELETE = requireAdmin(
-  async (_req: Request, params: { userId: string; apiKey: string }) => {
-    const apiKey = await getUserApiKey(params.userId, params.apiKey)
-    if (!apiKey) {
-      return ApiResponses.noSuchEntity(
-        `There is no api key with id ${params.apiKey} for user ${params.userId}`
-      )
-    }
-    if (apiKey.provisioned) {
-      return ApiResponses.forbiddenAction("Can't delete a provisioned api key")
-    }
-
-    try {
-      const result = await deleteApiKey(params.userId, params.apiKey)
-      if (result[0].numDeletedRows.toString() !== '1') {
+export const { DELETE } = route({
+  DELETE: operation({
+    name: 'Delete user API key',
+    description: 'Delete a specific API key for a user.',
+    authentication: 'admin',
+    implementation: async (_req: Request, params: { userId: string; apiKey: string }) => {
+      const apiKey = await getUserApiKey(params.userId, params.apiKey)
+      if (!apiKey) {
         return ApiResponses.noSuchEntity(
-          `No such api key ${params.apiKey} for user ${params.userId}`
+          `There is no api key with id ${params.apiKey} for user ${params.userId}`
         )
       }
-    } catch (e) {
-      const interpretedException = interpretDbException(e)
-      if (
-        interpretedException instanceof KnownDbError &&
-        interpretedException.code === KnownDbErrorCode.CONSTRAINT_FOREIGN_KEY
-      ) {
-        return ApiResponses.foreignKey('User has some activitity which is not deletable')
+      if (apiKey.provisioned) {
+        return ApiResponses.forbiddenAction("Can't delete a provisioned api key")
       }
-      return defaultErrorResponse(interpretedException)
-    }
-    return ApiResponses.success()
-  }
-)
+
+      try {
+        const result = await deleteApiKey(params.userId, params.apiKey)
+        if (result[0].numDeletedRows.toString() !== '1') {
+          return ApiResponses.noSuchEntity(
+            `No such api key ${params.apiKey} for user ${params.userId}`
+          )
+        }
+      } catch (e) {
+        const interpretedException = interpretDbException(e)
+        if (
+          interpretedException instanceof KnownDbError &&
+          interpretedException.code === KnownDbErrorCode.CONSTRAINT_FOREIGN_KEY
+        ) {
+          return ApiResponses.foreignKey('User has some activitity which is not deletable')
+        }
+        return defaultErrorResponse(interpretedException)
+      }
+      return ApiResponses.success()
+    },
+  }),
+})
--- logicle/app/api/users/[userId]/apiKeys/route.ts
+++ logicle/app/api/users/[userId]/apiKeys/route.ts
@@ -1,39 +1,48 @@
-import { getUserById } from '@/models/user'
-import { requireAdmin } from '@/api/utils/auth'
 import ApiResponses from '@/api/utils/ApiResponses'
-import * as dto from '@/types/dto'
 import { createApiKey, getUserApiKeys } from '@/models/apikey'
 import { nanoid } from 'nanoid'
 import { hashPassword } from '@/lib/auth'
+import { getUserById } from '@/models/user'
+import { route, operation } from '@/lib/routes'
+import { apiKeySchema, insertableUserApiKeySchema } from '@/types/dto'
 
-export const GET = requireAdmin(async (_req: Request, params: { userId: string }) => {
-  const user = await getUserById(params.userId)
-  if (!user) {
-    return ApiResponses.noSuchEntity(`There is no user with id ${params.userId}`)
-  }
-  const apiKeys: Omit<dto.ApiKey, 'key'>[] = (await getUserApiKeys(params.userId)).map((apiKey) => {
-    return {
-      ...apiKey,
-      key: '<hidden>',
-    }
-  })
-  return ApiResponses.json(apiKeys)
-})
-
-export const POST = requireAdmin(async (req: Request, params: { userId: string }) => {
-  const result = dto.insertableUserApiKeySchema.safeParse(await req.json())
-  if (!result.success) {
-    return ApiResponses.invalidParameter('Invalid body', result.error.format())
-  }
-  const user = await getUserById(params.userId)
-  if (!user) {
-    return ApiResponses.noSuchEntity(`There is no user with id ${params.userId}`)
-  }
-  const key = nanoid()
-  const hashed = await hashPassword(key)
-  const apiKey = await createApiKey(params.userId, hashed, result.data)
-  return ApiResponses.created({
-    ...apiKey,
-    key: key,
-  })
+export const { GET, POST } = route({
+  GET: operation({
+    name: 'List user API keys',
+    description: 'Fetch all API keys for a user.',
+    authentication: 'admin',
+    responseBodySchema: apiKeySchema.array(),
+    implementation: async (_req: Request, params: { userId: string }) => {
+      const user = await getUserById(params.userId)
+      if (!user) {
+        return ApiResponses.noSuchEntity(`There is no user with id ${params.userId}`)
+      }
+      return (await getUserApiKeys(params.userId)).map((apiKey) => {
+        return {
+          ...apiKey,
+          key: '<hidden>',
+        }
+      })
+    },
+  }),
+  POST: operation({
+    name: 'Create user API key',
+    description: 'Create a new API key for a user.',
+    authentication: 'admin',
+    requestBodySchema: insertableUserApiKeySchema,
+    responseBodySchema: apiKeySchema,
+    implementation: async (_req: Request, params: { userId: string }, { requestBody }) => {
+      const user = await getUserById(params.userId)
+      if (!user) {
+        return ApiResponses.noSuchEntity(`There is no user with id ${params.userId}`)
+      }
+      const key = nanoid()
+      const hashed = await hashPassword(key)
+      const apiKey = await createApiKey(params.userId, hashed, requestBody)
+      return {
+        ...apiKey,
+        key: key,
+      }
+    },
+  }),
 })
