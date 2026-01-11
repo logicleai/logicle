856ba8e7b5f0513aa89917f719e2ec37612ed9e6
Luca Piccarreta <luca.piccarreta@foosoft.it>
2026-01-11 17:32:03 +0100

    Revert "Chore: stable legacy message types (#716)"
    This reverts commit 8e15f7e8dbe256bb7bed5771a74c7eebb29d3530.

--- logicle/models/utils.ts
+++ logicle/models/utils.ts
@@ -1,6 +1,53 @@
 import * as schema from '@/db/schema'
 import * as dto from '@/types/dto'
-import { AssistantMessageV1, MessageV1 } from '@/types/legacy/messages-v1'
+
+export type BaseMessageV1 = Omit<schema.Message, 'role'> & {
+  attachments: dto.Attachment[]
+  citations?: dto.Citation[]
+}
+
+export type ToolCallMessageV1 = BaseMessageV1 &
+  dto.ToolCall & {
+    role: 'tool-call'
+    reasoning?: string
+    reasoning_signature?: string
+  }
+
+export type AssistantMessageV1 = BaseMessageV1 & {
+  role: 'assistant'
+  reasoning?: string
+  reasoning_signature?: string
+}
+
+export type ToolResultMessageV1 = BaseMessageV1 &
+  dto.ToolCallResult & {
+    role: 'tool-result'
+  }
+
+export type ToolOutputMessageV1 = BaseMessageV1 & {
+  role: 'tool-output'
+}
+
+export type ErrorMessageV1 = BaseMessageV1 & {
+  role: 'error'
+}
+
+export type DebugMessageV1 = BaseMessageV1 & {
+  role: 'tool-debug'
+  displayMessage: string
+  data: Record<string, unknown>
+}
+
+type MessageV1 =
+  | dto.UserMessage
+  | AssistantMessageV1
+  | ToolCallMessageV1
+  | ToolOutputMessageV1
+  | ToolResultMessageV1
+  | ErrorMessageV1
+  | DebugMessageV1
+  | dto.ToolCallAuthRequestMessage
+  | dto.ToolCallAuthResponseMessage
 
 export const parseV1 = (m: schema.Message) => {
   const content = m.content
